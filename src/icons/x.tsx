53fa2c0629d0e6eea155eb75f443b4f782523e06
Steve Konves <stephen.konves@gmail.com>
2020-12-21 16:19:09 -0700

    Add x icon

---
+++ src/icons/x.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { icon } from './base';
+
+export const X = icon(
+  'X',
+  <>
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </>,
+);
