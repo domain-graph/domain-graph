56927e190766d89f9bfc5d27c363427ca1930a0e
Steve Konves <stephen.konves@gmail.com>
2020-12-21 20:21:06 -0700

    Add icon-button

---
+++ src/components/icon-button.tsx
@@ -0,0 +1,44 @@
+import './icon-button.less';
+
+import React, { forwardRef, useMemo } from 'react';
+import { IconProps } from '../icons/base';
+
+export interface ButtonProps
+  extends React.ComponentPropsWithoutRef<'button'>,
+    IconProps {
+  Icon: React.VFC<IconProps>;
+}
+
+export const IconButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const {
+      className: originalClassName,
+      size,
+      color,
+      strokeWidth,
+      x,
+      y,
+      Icon,
+      ...rest
+    } = props;
+
+    const iconProps = {
+      size,
+      color,
+      strokeWidth,
+      x,
+      y,
+    };
+
+    const className = useMemo(
+      () => ['c-icon-button', originalClassName].filter((x) => x).join(' '),
+      [originalClassName],
+    );
+
+    return (
+      <button ref={ref} className={className} {...rest}>
+        <Icon {...iconProps} />
+      </button>
+    );
+  },
+);
