fafb04da1ffc0c56a44b4ef312ef9a722a9ad6b7
Steve Konves <stephen.konves@gmail.com>
2020-12-31 01:35:25 -0700

    Skip registry to fix build

--- src/registry.ts
+++ src/registry.ts
@@ -1,15 +1,15 @@
-import { Registry as GenericRegistry } from 'ts-registry';
-import { StateRepository } from './graph-state';
-import { StateService } from './graph-state/state-service';
+// import { Registry as GenericRegistry } from 'ts-registry';
+// import { StateRepository } from './graph-state';
+// import { StateService } from './graph-state/state-service';
 
-export type Registry = GenericRegistry<{
-  'state-repository': StateRepository;
-  'state-service': StateService;
-}>;
+// export type Registry = GenericRegistry<{
+//   'state-repository': StateRepository;
+//   'state-service': StateService;
+// }>;
 
-export function getRegistry(): Registry {
-  if (!global['window']['__REGISTRY__']) {
-    global['window']['__REGISTRY__'] = new GenericRegistry();
-  }
-  return global['window']['__REGISTRY__'];
-}
+// export function getRegistry(): Registry {
+//   if (!global['window']['__REGISTRY__']) {
+//     global['window']['__REGISTRY__'] = new GenericRegistry();
+//   }
+//   return global['window']['__REGISTRY__'];
+// }
