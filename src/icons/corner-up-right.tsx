af0c8ec2836bbb0b50fd8d111365eee2a3f0204e
Steve Konves <stephen.konves@gmail.com>
2021-01-01 16:09:02 -0700

    Add icon

---
+++ src/icons/corner-up-right.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { icon } from './base';
+
+export const CornerUpRight = icon(
+  'CornerUpRight',
+  <>
+    <polyline points="15 14 20 9 15 4"></polyline>
+    <path d="M4 20v-7a4 4 0 0 1 4-4h12"></path>
+  </>,
+);
