bc3fddc35e695d5a471b248750f33c967716179e
Steve Konves <stephen.konves@gmail.com>
2021-09-05 20:26:06 -0700

    Create toolbar

---
+++ src/graph/toolbar.tsx
@@ -0,0 +1,40 @@
+import './toolbar.less';
+
+import React, { useCallback } from 'react';
+import * as Icons from '../icons';
+import { useDispatch } from '../state';
+import { hideAllNodes, hideUnpinnedNodes } from '../state/graph/graph-actions';
+import { PillButton } from '../components/pill-button';
+
+export interface ToolbarProps {
+  onResetZoom(): void;
+  onFitAll(): void;
+}
+
+export const Toolbar: React.VFC<ToolbarProps> = ({ onResetZoom }) => {
+  const dispatch = useDispatch();
+  const handleHideAll = useCallback(() => {
+    dispatch(hideAllNodes() as any); // TODO: (issue: #40)
+    onResetZoom();
+  }, [dispatch, onResetZoom]);
+
+  const handleHideUnpinned = useCallback(() => {
+    dispatch(hideUnpinnedNodes() as any); // TODO: (issue: #40)
+  }, [dispatch]);
+  return (
+    <div className="c-toolbar">
+      <PillButton icon={Icons.EyeOff} onClick={handleHideAll}>
+        Hide all
+      </PillButton>
+      <PillButton icon={Icons.Unlock} onClick={handleHideUnpinned}>
+        Hide unpinned
+      </PillButton>
+      {/* <PillButton icon={Icons.Lock} onClick={onFitAll}>
+        Fit all
+      </PillButton> */}
+      <PillButton icon={Icons.Home} onClick={onResetZoom}>
+        Reset Zoom
+      </PillButton>
+    </div>
+  );
+};
