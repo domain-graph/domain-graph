424818f245167a631929ebf7ef2ec0bf149cdbf8
Steve Konves <stephen.konves@gmail.com>
2021-09-11 13:04:12 -0700

    Export function to parse introspection queries

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './data-provider';
 export * from './components/browser-open-file-dialog';
 export * from './persistence/local-storage-state-repository';
 export * from './persistence/types';
+export * from './tools/parser';
 export * as Icons from './icons';
